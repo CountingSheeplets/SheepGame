84e596a608cc16d58c4d63e746be9c2285298231
Danielius <carrotpie@gmail.com>
2020-01-03 13:34:41 +0200

    send score messages to all eliminated owners when new owner is eliminated

--- Assets/Scripts/Game/Scores/ScoreNetworkWriter.cs
+++ Assets/Scripts/Game/Scores/ScoreNetworkWriter.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class ScoreNetworkWriter : MonoBehaviour
 {
+    List<Owner> eliminatedOwners = new List<Owner>();
     void Start()
     {
         EventCoordinator.StartListening(EventName.System.Environment.EndMatch(), OnWin);
-        EventCoordinator.StartListening(EventName.System.Player.Eliminated(), OnLost);
+        EventCoordinator.StartListening(EventName.System.Player.Eliminated(), OnEliminated);
     }
     void OnDestroy(){
         EventCoordinator.StopListening(EventName.System.Environment.EndMatch(), OnWin);
-        EventCoordinator.StopListening(EventName.System.Player.Eliminated(), OnLost);
+        EventCoordinator.StopListening(EventName.System.Player.Eliminated(), OnEliminated);
     }
     void OnWin(GameMessage msg){
         PlayerScores playerScores = ScoreCoordinator.GetPlayerScores(msg.owner);
         NetworkCoordinator.SendPlayerScores(msg.owner.deviceId, true, playerScores.scores);
     }
-    void OnLost(GameMessage msg){
-        PlayerScores playerScores = ScoreCoordinator.GetPlayerScores(msg.targetOwner);
-        NetworkCoordinator.SendPlayerScores(msg.targetOwner.deviceId, false, playerScores.scores);
+    void OnEliminated(GameMessage msg){
+        if(!eliminatedOwners.Contains(msg.targetOwner))
+            eliminatedOwners.Add(msg.targetOwner);
+        foreach(Owner owner in eliminatedOwners){
+            PlayerScores playerScores = ScoreCoordinator.GetPlayerScores(owner);
+            NetworkCoordinator.SendPlayerScores(owner.deviceId, false, playerScores.scores);
+        }
     }
 }
