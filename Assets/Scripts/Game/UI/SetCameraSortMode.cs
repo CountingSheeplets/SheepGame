d9ddf7e3c59c40f97ea7ef8e9425e9c5e4b869cd
carrotpie <carrotpie@gmail.com>
2020-03-27 01:19:17 +0200

    version bump Unity to 2019.3.7f and SortingGroups for Spine objects.

---
+++ Assets/Scripts/Game/UI/SetCameraSortMode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCameraSortMode : MonoBehaviour {
+    void Start() {
+        Camera cam = Camera.main;
+        cam.transparencySortAxis = Vector3.up;
+        cam.transparencySortMode = TransparencySortMode.CustomAxis;
+    }
+}
