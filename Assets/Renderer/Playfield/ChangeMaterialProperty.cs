9c4b8c97686cdbb344f27b23b55626f68df4b03e
Danielius <carrotpie@gmail.com>
2020-07-07 16:26:18 +0300

    import files for playfield rendering through graph

---
+++ Assets/Renderer/Playfield/ChangeMaterialProperty.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialProperty : MonoBehaviour {
+    bool trigger;
+    float blendSpeed = 0.5f;
+    float visibility = 1f;
+    SpriteRenderer rend;
+
+    void Start() {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.A)) {
+            Debug.Log("Start ChangeMaterialProperty");
+            trigger = true;
+            visibility = 1f;
+            rend.material.SetFloat("_CurrentLevel", 1);
+        }
+        if (trigger) {
+            visibility -= Time.deltaTime * blendSpeed;
+            if (visibility >= 0)
+                rend.material.SetFloat("_CurrentLevel", visibility);
+            else {
+                rend.material.SetFloat("_CurrentLevel", 0);
+                trigger = false;
+            }
+        }
+    }
+}
