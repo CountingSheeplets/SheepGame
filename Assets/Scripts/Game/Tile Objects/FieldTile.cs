cab1b5122d1f40da937d18a322dac0b0c976d557
Danielius <carrotpie@gmail.com>
2020-07-09 17:11:07 +0300

    display playfield via shader graph

--- Assets/Scripts/Game/Tile Objects/FieldTile.cs
+++ Assets/Scripts/Game/Tile Objects/FieldTile.cs
@@ -2,40 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FieldTile : BaseTile
-{
-    public SubFieldTile[,] subTiles = new SubFieldTile[2,2];
+public class FieldTile : BaseTile {
+    public SubFieldTile[, ] subTiles = new SubFieldTile[2, 2];
     public bool isGrass = false;
     //public List<string> listenerNames = new List<string>();
-    public void SetState(bool state){
+    public void SetState(bool state) {
         isGrass = state;
         FieldTileSpriteType stateType = FieldTileSpriteType.any;
-        if(state){
+        if (state) {
             stateType = FieldTileSpriteType.grass;
         } else {
             stateType = FieldTileSpriteType.mud;
         }
-        if(onListenStateChange != null)
+        if (onListenStateChange != null)
             onListenStateChange(this, stateType);
     }
-    void Awake(){
+    void Awake() {
         List<SubFieldTile> subs = new List<SubFieldTile>(GetComponentsInChildren<SubFieldTile>());
         int index = 0;
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 2; j++){
-                subTiles[j,i] = subs[index];//order is important
-                index ++;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                subTiles[j, i] = subs[index]; //order is important
+                index++;
             }
         }
     }
-    public delegate void OnStateChange (FieldTile tile, FieldTileSpriteType state);
+    public delegate void OnStateChange(FieldTile tile, FieldTileSpriteType state);
     public OnStateChange onListenStateChange;
     public Dictionary<FieldTile, Location3x3> neighbours = new Dictionary<FieldTile, Location3x3>();
     public List<NeighbourItem> neighbTest = new List<NeighbourItem>();
 
-    public void SubscribeToNeighbour(FieldTile tile, Location3x3 loc3x3){
-        if(tile != null){
-            if(!neighbours.ContainsKey(tile) && !loc3x3.IsCenterTile()){
+    public void SubscribeToNeighbour(FieldTile tile, Location3x3 loc3x3) {
+        if (tile != null) {
+            if (!neighbours.ContainsKey(tile) && !loc3x3.IsCenterTile()) {
                 neighbTest.Add(new NeighbourItem(tile, new Vector2(loc3x3.y, loc3x3.x)));
                 neighbours.Add(tile, loc3x3.ToLocal());
                 tile.onListenStateChange += OnNeighbourStateChanged;
@@ -44,37 +43,42 @@ public class FieldTile : BaseTile
         } else {
             FillWater(loc3x3.ToLocal());
         }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                subTiles[j, i].SetupSubTile(transform.localPosition.x, transform.localPosition.y);
+            }
+        }
     }
 
-    void OnNeighbourStateChanged(FieldTile tile, FieldTileSpriteType newState){
+    void OnNeighbourStateChanged(FieldTile tile, FieldTileSpriteType newState) {
         //Debug.Log("calling OnNeightbourh:"+gameObject.name+" by: "+tile.gameObject.name);
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 2; j++){
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
                 //Debug.Log("affec check: "+subTiles[i,j]+" loc3: "+neighbours[tile]+" "+subTiles[i,j].IsAffected(neighbours[tile]));
-                if(subTiles[i,j].IsAffected(neighbours[tile])){
+                if (subTiles[i, j].IsAffected(neighbours[tile])) {
                     Location2x2 loc2x2 = new Location2x2(neighbours[tile], i, j);
                     //Debug.Log(subTiles[i,j]+" no exp: "+subTiles[i,j].GetState());
                     //Debug.Log(subTiles[i,j]+" before: "+subTiles[i,j].ExportState());
-                    subTiles[i,j].SetSubState(loc2x2, newState);
+                    subTiles[i, j].SetSubState(loc2x2, newState);
                     //Debug.Log(subTiles[i,j]+" after:"+subTiles[i,j].ExportState());
                 }
             }
         }
     }
 
-    public void FillWater(Location3x3 loc3x3){
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 2; j++){
-                if(subTiles[i,j].IsAffected(loc3x3)){
-                    TileSpriteState newTileState = new TileSpriteState(i,j);
+    public void FillWater(Location3x3 loc3x3) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (subTiles[i, j].IsAffected(loc3x3)) {
+                    TileSpriteState newTileState = new TileSpriteState(i, j);
                     Location2x2 loc2x2 = new Location2x2(loc3x3, i, j);
-                    subTiles[i,j].SetSubState(loc2x2, FieldTileSpriteType.water);
+                    subTiles[i, j].SetSubState(loc2x2, FieldTileSpriteType.water);
                 }
             }
         }
     }
-    void OnDestroy(){
-        foreach(FieldTile tile in neighbours.Keys)
+    void OnDestroy() {
+        foreach (FieldTile tile in neighbours.Keys)
             tile.onListenStateChange -= OnNeighbourStateChanged;
     }
 }
@@ -83,7 +87,7 @@ public class FieldTile : BaseTile
 public struct NeighbourItem {
     public FieldTile tileNeighb;
     public Vector2 position;
-    public NeighbourItem(FieldTile tile, Vector2 pos){
+    public NeighbourItem(FieldTile tile, Vector2 pos) {
         tileNeighb = tile;
         position = pos;
     }
--- Assets/Scripts/Game/Tile Objects/Objects/SubFieldTile.cs
+++ Assets/Scripts/Game/Tile Objects/Objects/SubFieldTile.cs
@@ -8,6 +8,7 @@ public class SubFieldTile : MonoBehaviour {
     //public SpriteMask myMask;
     bool isDirty = true;
     FieldTileSpriteType myParentStateType;
+    Material myMaterial;
     public FieldTileSpriteType GetParentType() {
         return myParentStateType;
     }
@@ -62,10 +63,41 @@ public class SubFieldTile : MonoBehaviour {
     }
 
     void SpriteSet() {
-        mySprite.sprite = TileSpriteFactory.GetSprite(this);
+        Sprite spr = TileSpriteFactory.GetSprite(this);
+        //mySprite.sprite = spr;
+        // get coordinates of sprite and set it onto SpriteRenderer
         //myMask.sprite = mySprite.sprite;
+        //string outp = " ";
+        //foreach (Vector2 uv in spr.uv)
+        //    outp += uv + " ";
+        //Debug.Log(transform.parent.name + "  -outp uvs-" + spr.rect);
+        Vector2 offset = new Vector2(spr.rect.x / 896, spr.rect.y / 896);
+        myMaterial.SetVector("_GrassOffset", offset);
     }
+    public void SetupSubTile(float x, float y) {
+        float ts = ConstantsBucket.PlayfieldTileSize;
+        float gs = ConstantsBucket.GridSize;
+        float tilingDif = 1f / (ConstantsBucket.GridSize);
+        Vector2 tiling = new Vector2(tilingDif / 2f, tilingDif / 2f);
+        myMaterial = GetComponent<SpriteRenderer>().material;
+        myMaterial.SetVector("_CrackTiling", tiling);
+        float subX = 0f;
+        float subY = 0f;
+        float dif = (gs - 1f) / 2f;
+        if (transform.localPosition.x > 0)
+            subX = tilingDif / 2f;
+        if (transform.localPosition.y > 0)
+            subY = tilingDif / 2f;
+        float tileX = x / ts + dif;
+        float tileY = y / ts + dif;
+        //this is position at the moment, which is wrong
+        float div = gs * ts;
+        //must be <1, so divide it by max and calculate off scale, because position is with minus
+        //then also add another push because of subtile offsset under Tile obj(+ -0.5)
+        Vector2 offset = new Vector2((tileX) / gs + subX, (tileY) / gs + subY);
+        myMaterial.SetVector("_CrackOffset", offset);
 
+    }
     public bool IsAffected(Location3x3 loc3x3) {
         if (loc3x3.IsCenterTile()) {
             return false;
