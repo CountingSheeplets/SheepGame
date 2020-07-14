f6ecacb5542ee8d2417b83c342013b3ccc35ea13
Danielius <carrotpie@gmail.com>
2020-07-14 15:39:23 +0300

    onDeath king disappears eased out via Easing.Cubic.InOut(alpha);

--- Assets/Scripts/Game/King/Ability Handlers/KingAnimateDeath.cs
+++ Assets/Scripts/Game/King/Ability Handlers/KingAnimateDeath.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Spine.Unity;
 using UnityEngine;
 public class KingAnimateDeath : MonoBehaviour {
     public Owner owner;
     public Animator anim;
-
+    bool trigger;
+    Spine.Skeleton skeleton;
+    Color color;
+    float alpha;
+    void Awake() {
+        skeleton = GetComponent<SkeletonMecanim>().skeleton;
+        color = skeleton.GetColor();
+        alpha = color.a + 0.1f;
+        Debug.Log(skeleton.GetColor());
+    }
     void Start() {
         anim = GetComponent<Animator>();
         if (GetComponentInParent<KingUnit>()) {
@@ -24,11 +34,21 @@ public class KingAnimateDeath : MonoBehaviour {
         Debug.Log("my owner: " + owner);
         if (owner.EqualsByValue(msg.targetOwner)) {
             anim.SetTrigger("die");
+            trigger = true;
         }
         List<BaseUnitMove> moves = new List<BaseUnitMove>(GetComponentsInParent<BaseUnitMove>().Where(x => !(x is FieldFloat)));
         foreach (BaseUnitMove move in moves) {
             Destroy(move);
         }
-
+    }
+    void Update() {
+        if (trigger) {
+            alpha -= Time.deltaTime / ConstantsBucket.PlayfieldFadeTime;
+            float eased = Easing.Cubic.InOut(alpha);
+            if (alpha > 0) {
+                skeleton.SetColor(new Color(color.r, color.g, color.b, eased));
+                Debug.Log(skeleton.GetColor());
+            } else trigger = false;
+        }
     }
 }
